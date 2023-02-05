13ab076908b0e3c80e5fb1952574be62301bcdc3
Azajul-Karim <azajulkarim2021@gmail.com>
2023-02-05 08:21:29 +0600

    Class 3 Added

---
+++ Day 4/index.js
@@ -0,0 +1,62 @@
+//String
+"hello world";
+"hello world";
+
+console.log(typeof "hello world");
+
+// concat
+"Azajul" + "Karim"; // Azajul Karim
+
+//Eacape Char start With "\"
+var escape1 = "Hello I 'Am Azajul";
+console.log(escape1);
+
+//String Lrngth
+"Hello World".length; //11
+
+//Access Individual Characters Using [] And An Index
+"Hello World"[0]; //"h"
+"Hello World"[4]; //"o"
+
+//number
+123;
+3.334 - 2344;
+2345;
+
+console.log(typeof 123);
+console.log(typeof 3.334);
+console.log(typeof -2344);
+
+//We Can Do Some Math
+4 + 10; //14
+10 - 6; //4
+10 / 2; //5
+3 * 5; //15
+3 ** 3; //26
+
+//Modulos - Remainder Operator
+10 % 3; //1
+24 % 2; //0
+
+// Boolean
+
+true;
+false;
+
+console.log(typeof true);
+console.log(typeof false);
+
+// Symbol
+Symbol("rand1");
+Symbol("rand2");
+
+console.log(typeof Symbol("rand1"));
+console.log(typeof Symbol("rand2"));
+
+// Null Undefined
+
+//Big Int
+123n;
+456n;
+
+console.log(typeof 123n);
