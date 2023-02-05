c5b58c8b9ce7856e3faf6b898917958489d9dfe2
Azajul-Karim <azajulkarim2021@gmail.com>
2023-02-05 18:57:11 +0600

    Day 4 Added

---
+++ Day 4/exersize.js
@@ -0,0 +1,54 @@
+// Task 1
+let name = "Adnan";
+let email = "samimfaz@gmail.com";
+let age = 30;
+let isDeveloper = true;
+let goingToBeDesigner = null;
+
+// Task 2
+console.log(
+  `Hey it's me ${name}. I am ${age}.you can contact me by this ${email}.I am a Developer and it's ${isDeveloper}. I am going to be a Designer ${goingToBeDesigner}`
+);
+//Task 3
+let fruit = "Mango";
+fruit = "Apple";
+console.log(fruit);
+
+// Task 4
+
+// ### Undefined ###
+/*
+1.Variables That Are Declared But Not Initialized Are undefined
+2.The Following Variables Are Undefined
+3.It is not an assignment value. It means a variable has been declared but has not yet been assigned a value.
+4.It is a type itself.
+5.The undefined value is a primitive value, which is used when a variable has not been assigned a value.
+6.Undefined indicates the absence of the variable itself.
+7.Undefined is converted to NaN while performing primitive operations.
+*/
+// ### Null ###
+
+/*
+1.Null Is Explicityly Nothing
+2.It is an assignment value. It can be assigned to a variable which indicates that a variable does not point any object.
+3.It is an object.
+4.The null value is a primitive value which represents the null, empty, or non-existent reference.
+5.Null indicates the absence of a value for a variable.
+6.Null is converted to zero (0) while performing primitive operations.
+*/
+
+// Task 5
+/*
+In JavaScript, NaN is short for "Not-a-Number".
+
+In JavaScript, NaN is a number that is not a legal number.
+
+the typeof NaN number
+*/
+
+// Task 6
+
+let things;
+console.log(things);
+
+// the value of thing undefined
--- Day 4/index.js
+++ Day 4/index.js
@@ -60,3 +60,42 @@ console.log(typeof Symbol("rand2"));
 456n;
 
 console.log(typeof 123n);
+
+var firstName = "Topu";
+
+console.log(firstName + " Ahmed");
+
+let lastName = "Ahmed";
+
+console.log(firstName + " " + lastName);
+
+const profession = "Software Developer";
+console.log(firstName + " " + lastName + " is a " + profession);
+
+console.log(`${firstName} ${lastName} is a ${profession}`);
+
+// null and undefined
+let var1;
+console.log(var1);
+let var2 = null;
+console.log(var2);
+// ### Undefined ###
+/*
+1.Variables That Are Declared But Not Initialized Are undefined
+2.The Following Variables Are Undefined
+3.It is not an assignment value. It means a variable has been declared but has not yet been assigned a value.
+4.It is a type itself.
+5.The undefined value is a primitive value, which is used when a variable has not been assigned a value.
+6.Undefined indicates the absence of the variable itself.
+7.Undefined is converted to NaN while performing primitive operations.
+*/
+// ### Null ###
+
+/*
+1.Null Is Explicityly Nothing
+2.It is an assignment value. It can be assigned to a variable which indicates that a variable does not point any object.
+3.It is an object.
+4.The null value is a primitive value which represents the null, empty, or non-existent reference.
+5.Null indicates the absence of a value for a variable.
+6.Null is converted to zero (0) while performing primitive operations.
+*/
