0ebec20e457d15509211b77615715762161da6d6
Azajul-Karim <azajulkarim2021@gmail.com>
2023-02-08 22:48:17 +0600

    Day 8 Exercize Added

---
+++ Day 8/index.js
@@ -0,0 +1,39 @@
+/*
+Arithmetic Operator
+#Addition (+)
+#Subtraction (-)
+#Multiplication (*)
+#Division (/)
+#Raising To Power(**)
+*/
+
+// Assignment Operator (=)
+let num1 = 20;
+let num2 = 5;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+
+let num4 = 6;
+// num4 = num4 + 4;
+num4 += 4; //Shortcurt
+num4++; //Post Increment
+++num4; //Pre Increment
+
+// num4 = num4 - 4;
+num4 -= 4; //Shortcurt
+num4--; //Post Decrement
+--num4; //Pre Decrement
+console.log(num4);
+
+let num3 = 3;
+console.log(num3++); //Post Decrement
+console.log(++num3); //Pre Decrement
+
+let num5 = 5;
+console.log(num3--); //Post Decrement
+console.log(--num3); //Pre Decrement
