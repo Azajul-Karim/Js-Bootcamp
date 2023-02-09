bf55c1d812193fa76460953706959ea4fc6be326
Azajul-Karim <azajulkarim2021@gmail.com>
2023-02-09 08:35:11 +0600

    Day 8 All Exercise Added

--- Day 8/index.js
+++ Day 8/index.js
@@ -37,3 +37,72 @@ console.log(++num3); //Pre Decrement
 let num5 = 5;
 console.log(num3--); //Post Decrement
 console.log(--num3); //Pre Decrement
+
+/*
+Comparison OPerator
+let x = 6
+Greater Than (>)
+x > 10 - false
+
+Greater Than Or Equal To (>=)
+x >= 6 - true
+
+Less Than (<)
+x < 50 - false
+
+Less Than Or Equal To (<=)
+x <= 100 - true
+
+Equal To (==)
+x == "6" - true
+
+Strict Equal To (===)
+x === "6" - false
+
+Not Equal To (!=)
+x != "b" - true
+*/
+
+// == vs ===
+// Loose Equal (Value)
+//Strict Equal (Type + Value)
+
+let x = 6;
+console.log(x > 10);
+console.log(x >= 6);
+console.log(x < 50);
+console.log(x <= 100);
+console.log(x == "6");
+console.log(x === "6");
+console.log(x != "b");
+
+// == vs ===
+x == "6"; //Only Check Value (True)
+x === "6"; //Not Only Check Value But Also Type(False) (True)
+
+/*
+x = 5 and y = 9
+Logical Operator
+
+Logical And Operator (&&)
+x < 10 && x !== 5 -false
+
+Logical Or Operator (||)
+y > 9 || x === 5 - true
+
+Logical Not Operator (!)
+!(x === y) - true
+*/
+
+let a = 5;
+let b = 9;
+
+//Logical AND Return True When Every Part is True  otherwise False
+
+console.log(a > 10 && b !== 5); //short circuit
+
+//Logical AND Return True When Any Part is True  otherwise False
+console.log(a < 10 || b !== 5); //short circuit
+
+//Logical Not
+console.log(!(a === b));
