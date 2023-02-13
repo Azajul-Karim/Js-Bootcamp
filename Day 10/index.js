6ed19d910d2e688fe757513404f7df40ce760a06
Azajul-Karim <azajulkarim2021@gmail.com>
2023-02-13 19:08:05 +0600

    all task added

--- Day 10/index.js
+++ Day 10/index.js
@@ -5,19 +5,19 @@
  }
 */
 
-let count = 0;
-while (count < 7) {
-  console.log(`i love bangladesh ${count + 1} times`);
-  count++;
-}
+// let count = 0;
+// while (count < 7) {
+//   console.log(`i love bangladesh ${count + 1} times`);
+//   count++;
+// }
 
-while (count < 7) {
-  const realCount = count + 1;
-  console.log(
-    `i love bangladesh ${realCount} ${realCount < 2 ? "time" : "times"} `
-  );
-  count++;
-}
+// while (count < 7) {
+//   const realCount = count + 1;
+//   console.log(
+//     `i love bangladesh ${realCount} ${realCount < 2 ? "time" : "times"} `
+//   );
+//   count++;
+// }
 
 // Infinity loop
 // while (count < 10) {
@@ -37,32 +37,83 @@ for(init; condition; step){
    run some code
 }
 */
-for (let i = 0; i < 10; i++) {
-  console.log(i);
-}
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
 
 //reverse loop
-for (let i = 10; i >= 0; i--) {
-  console.log(i);
-}
+// for (let i = 10; i >= 0; i--) {
+//   console.log(i);
+// }
 
-let i = 10;
-while (i >= 0) {
-  console.log(i);
-  i--;
-}
+// let i = 10;
+// while (i >= 0) {
+//   console.log(i);
+//   i--;
+// }
 
 //break continue
-for (let i = 0; i < 10; i++) {
-  if (i === 5) {
-    continue;
-  }
-  console.log(i);
-}
+// for (let i = 0; i < 10; i++) {
+//   if (i === 5) {
+//     continue;
+//   }
+//   console.log(i);
+// }
 
-for (let i = 0; i < 10; i++) {
-  if (i === 5) {
-    break;
+// for (let i = 0; i < 10; i++) {
+//   if (i === 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+//nested loop - for every step of the parent loop inner loop will complete
+// for (let i = 0; i < 3; i++) {
+//   console.log(i);
+//   for (let j = 0; j < 3; j++) {
+//     console.log(i, j);
+//   }
+// }
+
+//looping with string
+// let str = "hello";
+
+// for (let i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
+
+//loop array and object
+// let profile = ["topu", 23, "developer"];
+// for (let i = 0; i < profile.length; i++) {
+//   console.log(i);
+//   console.log(profile[i]);
+// }
+
+// let profile1 = {
+//   name: "topu",
+//   age: 30,
+//   profession: "developer",
+// };
+
+// for (const property in profile1) {
+//   console.log(profile1[property]);
+// }
+
+// for of loop vs for in loop
+// let profile = ["topu", 23, "developer"];
+// for (const elm of profile) {
+//   console.log(elm);
+// }
+
+//for of loop - array, string
+//for of loop - object
+//for in loop can be use with string and array but its not recommend as it does not ensure the order
+
+// loop inside function
+let names = ["topu", "sharif", "samim"];
+function printNames() {
+  for (const name of names) {
+    console.log(name);
   }
-  console.log(i);
 }
+printNames();
---
+++ Day 10/task.js
@@ -0,0 +1,43 @@
+// task 1
+// let num = 1;
+// while (num <= 10) {
+//   console.log(num);
+//   num += 2;
+// }
+//The output of the code will be:(1,3,5,7,9)
+/*
+Explanation:
+The code sets the variable num to 1, and then enters a while loop. The while loop continues as long as num is less than or equal to 10.
+
+In each iteration of the loop, the current value of num is logged to the console using console.log(num), and then num is incremented by 2 using num += 2.
+
+The loop continues until num is greater than 10, at which point the loop stops and the code execution is complete.
+*/
+
+// task 2
+// let num = 1;
+// while (num <= 20) {
+//   if (num % 4 === 0) {
+//     console.log(num);
+//   }
+//   num++;
+// }
+/*
+The output of the code will be:(4,8,12,16,20)
+
+Explanation:
+The code sets the variable num to 1, and then enters a while loop. The while loop continues as long as num is less than or equal to 20.
+
+In each iteration of the loop, a check is made to see if the current value of num is divisible by 4 using num % 4 === 0. If num is divisible by 4, the current value of num is logged to the console using console.log(num).
+
+Regardless of whether num is divisible by 4 or not, num is incremented by 1 using num++.
+
+The loop continues until num is greater than 20, at which point the loop stops and the code execution is complete.
+*/
+
+// task 3
+// let num = 100;
+// while (num < 150) {
+//   console.log(num + 1);
+//   num--;
+// }
