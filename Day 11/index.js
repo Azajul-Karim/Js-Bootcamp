319d3487c4eec0b37d7b5d08d7d1391e3679bd4a
Azajul-Karim <azajulkarim2021@gmail.com>
2023-02-14 08:27:21 +0600

    Day 11 Practice Class Added

---
+++ Day 11/index.js
@@ -0,0 +1,13 @@
+//Javascript built in properties and methods
+let str = "Hello There";
+
+console.log(str.length);
+console.log(str.toLocaleUpperCase());
+console.log(str.toLocaleLowerCase());
+console.log(str.indexOf("l"));
+console.log(str.lastIndexOf("l"));
+console.log(str.trim());
+console.log(str.trimStart());
+console.log(str.includes("Hello"));
+console.log(str.startsWith("Hello"));
+console.log(str.endsWith("There"));
