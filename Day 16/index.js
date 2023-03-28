ee8f0f330b43831b13ba600001dc7c8133630eb5
Azajul-Karim <azajulkarim2021@gmail.com>
2023-03-28 15:55:50 +0600

    array helper method added

--- Day 16/index.js
+++ Day 16/index.js
@@ -2,8 +2,80 @@
 
 const arr = [1, 2, 3, 4, 5];
 
-//map
-const result = arr.map(function (elm) {
-  return elm;
-});
-console.log(result);
+//Map
+// const result = arr.map(function (elm) {
+//   return elm;
+// });
+
+// console.log(result);
+
+//  Filter
+// const result = arr.filter((elm) => elm % 2 === 0);
+
+// console.log(result);
+
+//Find
+// const result = arr.find((elm) => elm % 2 === 0);
+
+// console.log(result);
+
+//findIndex
+// const result = arr.findIndex((elm) => elm % 2 === 0);
+
+// console.log(result);
+
+// Every
+// const result = arr.every((elm) => elm % 2 === 0);
+
+// console.log(result);
+
+//Some
+// const result = arr.some((elm) => elm % 2 === 0);
+
+// console.log(result);
+
+// Reduce
+// const result = arr.reduce((elm, curr) => {
+//   console.log(elm, curr);
+//   return elm + curr;
+// }, 0);
+
+// console.log(result);
+
+const friends = [
+  {
+    name: "topu",
+    books: ["bible", "harry potter"],
+    age: 21,
+  },
+  {
+    name: "nazim",
+    books: ["war of pace", "harry potter two"],
+    age: 22,
+  },
+  {
+    name: "rahi",
+    books: ["the lord of ring", "the shining"],
+    age: 23,
+  },
+];
+
+const allBooks = friends.reduce((acc, curr) => {
+  return [...acc, curr.books];
+}, []);
+
+console.log(allBooks);
+
+// countName
+const names = ["Topu", "Nazim", "Rahi", "Topu", "Nazim"];
+
+const countName = names.reduce((allNames, name) => {
+  if (name in allNames) {
+    allNames[name] = allNames[name] + 1;
+  } else {
+    allNames[name] = 1;
+  }
+  return allNames;
+}, {});
+
+console.log(countName);
